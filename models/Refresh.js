3ebf590a5092ecd043084ea7c16f57b4b6c68f3c
Jewells Joshi <jewellsjoshi437@gmail.com>
2020-11-20 10:32:18 +0530

    Added APIs

---
+++ models/Refresh.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const refreshSchema = new Schema(
+  {
+    token: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Refresh = mongoose.model("Refresh", refreshSchema);
+
+module.exports = Refresh;
